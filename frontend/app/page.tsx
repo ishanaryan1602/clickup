741ba1001f94a6bc37a3020606737bc34139082e
ishanaryan1602 <github1602@gmail.com>
2025-10-29 18:55:43 +0530

    initial commit

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
     <div className="h-screen " >
       <Navbar />
       <HeroBanner />
+      
     </div>
   );
 }
--- frontend/components/custom/heroBanner/heroBanner.tsx
+++ frontend/components/custom/heroBanner/heroBanner.tsx
@@ -5,11 +5,15 @@ const HeroBanner = () => {
         <div className='herobanner_outer_div'>
             <h1 className='herobanner_h1_heading' >The everything app, for work</h1>
             <p className='herobanner_paragraph_below_h1' >One app for projects, knowledge, conversations and more.
-                <br/>
+                <br />
                 Get more done, faster—together.</p>
-            <button className='herobanner_get_started_button' >
-                Get started. It's FREE!
-            </button>
+            <div className='herobanner_get_started_button_outer_div2'>
+                <div className='herobanner_get_started_button_outer_div1'>
+                    <button className='herobanner_get_started_button' >
+                        Get started. It's FREE!
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }
